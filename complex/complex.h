7447a1fa0b423bb47850cdf942c729f95aa7b36a
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2012-03-09 22:35:41 +0100

    Operacje na liczbach zespolonych

--- complex/complex.h
+++ complex/complex.h
@@ -3,6 +3,7 @@
  
 #include <sstream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -48,17 +49,84 @@ template <class TYP> class Complex {
 			return ss.str();
 		}
 
+		//potêga: ujemna lub dodatnia, 
+		// tylko liczby ca³kowite 
 		Complex<TYP> power(unsigned times){
-			//procedura podnoszenia do potegi przez wielokrotne mno¿enie 
 			if(times == 0){
 				return 1;
 			}else{
-				Complex<TYP> res = Complex(re, im);
-				for(int i=1; i<times; i++){
-					res = *this * res;
+				//procedura podnoszenia do potegi przez wielokrotne mno¿enie 
+				if(times > 0){
+					Complex<TYP> res = Complex(re, im);
+					for(int i=1; i<times; i++){
+						res = *this * res;
+					}
+					return res;
+				}else{
+					//x^(-a) = (1/x)^a
+					Complex one = new Complex(1,0);	//1 + 0i
+					Complex cpl = one / *this;
+					return cpl.power(-times);
 				}
-				return res;
 			}
+		}
+
+		Complex<TYP> power(float times){
+			if(times == 0){
+				return 1;
+			}else{
+				//procedura podnoszenia do potegi przez wielokrotne mno¿enie 
+				if(times > 0){
+					Complex<TYP> res = Complex(re, im);
+					for(int i=1; i<times; i++){
+						res = *this * res;
+					}
+					return res;
+				}else{
+					//x^(-a) = (1/x)^a
+					Complex one = new Complex(1,0);	//1 + 0i
+					Complex cpl = one / *this;
+					return cpl.power(-times);
+				}
+			}
+		}
+
+		//wzór de Moivre
+		Complex[] root(int n){
+			Complex* arr = new (nothrow) Complex[n];
+			double z = sqrt(pow(re,2.0) + pow(im,2.0));
+			double _z = pow(abs(z), -i);
+
+			for(int i=0; i<n; i++){	
+				double _re = cos((fi() + 2*i*M_PI)/n);
+				_re = _z * _re;
+
+				double _im = sin((fi() + 2*i*M_PI)/n);
+				_im = _z * _im;
+
+				arr[i] = Complex(_re, _im);
+			}
+			return arr;
+		}
+
+		//znajduje wartoœæ fi
+		double fi(){
+			if( re>0 ){	
+				return atan(im/re);
+			}
+			if( re<0 && im>=0 ){
+				return atan(im/re) + M_PI;
+			}
+			if( re<0 && im<0 ){
+				return atan(im/re) - M_PI;
+			}
+			if( re==0 && im>0 ){
+				return M_PI/2;
+			}
+			if( re==0 && im<0 ){
+				return -M_PI/2;
+			}
+			throw exception("Nie mo¿na obliczyæ fi");
 		}
 
 		//OPERATORY
